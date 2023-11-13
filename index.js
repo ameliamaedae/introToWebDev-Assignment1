61acb62c1ac441dc1fbf70474ddd8196e89e2418
Primbrook <68938110+Primbrook@users.noreply.github.com>
2023-11-13 07:55:34 -0600

    Enter Key

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
 
+function clickPress(event) {
+    if (event.key == "Enter") {
+        ponderFunc();
+    }
+}
+
 function ponderFunc() {
     let usrInput = document.getElementById("usrInput").value;
     if (!usrInput){
